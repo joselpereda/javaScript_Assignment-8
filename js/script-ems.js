e351929d8ee7ca705d1d40c909a1a633cee9d046
Jose Pereda <joselpereda@hotmail.com>
2023-04-11 13:15:41 -0700

    Assignment part 2 init commit

--- js/script-ems.js
+++ js/script-ems.js
@@ -1,59 +1,102 @@
-// CREATE AN ARRAY OF EMPLOYEES
+// SET STORAGE
+Storage.prototype.setObj = function(key, obj) {
+    return this.setItem(key, JSON.stringify(obj));
+};
 
+// GET STORAGE
+Storage.prototype.getObj = function(key) {
+    return JSON.parse(this.getItem(key));
+};
+
+// CREATE AN ARRAY OF EMPLOYEES
+let arrEmployees = [
+    [55532444, "Jose Pereda", 5541, "jose.pereda@ms.com", "Engineering Lead"],
+    [33454566, "Osman Vandicci", 6544, "osman.vandicci@ms.com", "Support Lead"],
+    [84747743, "Tito Stainbach", 9876, "tito.stanback@ms.com", "Research Dir"],
+    [23432410, "Angel Santiago", 1234, "angel.santiago@mscom", "Sales Dir"],
+    [78655433, "Karina Rosales", 6543, "karina.rosales@ms.com", "M&O Lead"]
+];
 
 // CHECK TO SEE IF STORAGE OBJECT EXISTS WHEN THE PAGE LOADS
-// IF DOES, RETURN STORAGE OBJECT INTO ARRAY INSTEAD OF POPULATED ARRAY
-
+// IF DOES, RETURN STORAGE OBJECT INTO ARRAY INSTEAD OF POPULATING ARRAY
+if (localStorage.getObj('employees') !== null) {
+    arrEmployees = localStorage.getObj('employees');
+}
+if (localStorage.getItem('employees') !== null) {
+    arrEmployees = JSON.parse(localStorage.getItem('employees'));
+}
 
 // GET DOM ELEMENTS
-
+let form        = document.querySelector('#addForm');
+let empTable    = document.querySelector('#employees');
+let empCount    = document.querySelector('#empCount');
 
 // BUILD THE EMPLOYEES TABLE WHEN THE PAGE LOADS
-
+buildGrid(arrEmployees);
 
 // ADD EMPLOYEE
 form.addEventListener('submit', (e) => {
     // PREVENT FORM SUBMISSION
-
+    e.preventDefault();
     // GET THE VALUES FROM THE TEXT BOXES
-
+    let empID       = parseInt(document.querySelector('#id').value);
+    let empName     = document.querySelector('#name').value;
+    let empExt      = parseInt(document.querySelector('#extension').value);
+    let empEmail    = document.querySelector('#email').value;
+    let empDept     = document.querySelector('#department').value;
     // ADD THE NEW EMPLOYEE TO A NEW ARRAY OBJECT
-
+    let arrNewEmployee = [empID, empName, empExt, empEmail, empDept];
     // PUSH THE NEW ARRAY TO THE *EXISTING* EMPLOYEES ARRAY
-
+    arrEmployees.push(arrNewEmployee);
     // BUILD THE GRID
-
+    buildGrid(arrEmployees);
     // RESET THE FORM
-
+    document.querySelector('#addForm').reset();
     // SET FOCUS BACK TO THE ID TEXT BOX
-
+    document.querySelector('#id').focus();
 });
 
 // DELETE EMPLOYEE
 empTable.addEventListener('click', (e) => {
-    // CONFIRM THE DELETE
-
-        // GET THE SELECTED ROWINDEX FOR THE TR (PARENTNODE.PARENTNODE)
-
-        // REMOVE EMPLOYEE FROM ARRAY
-
-        // BUILD THE GRID
-
+    if (e.target.classList.contains('delete')) {
+        // CONFIRM THE DELETE
+        if (confirm('Are you sure you want to delete this employee?')) {
+            // GET THE SELECTED ROWINDEX FOR THE TR (PARENTNODE.PARENTNODE)
+            let rowIndex = e.target.parentNode.parentNode.rowIndex;
+            // REMOVE EMPLOYEE FROM ARRAY
+            arrEmployees.splice(rowIndex - 1, 1);
+            // BUILD THE GRID
+            buildGrid(arrEmployees);
+        }
+    }
 });
 
 // BUILD THE EMPLOYEES GRID
-function buildGrid() {
+function buildGrid(arrEmployees) {
     // REMOVE THE EXISTING SET OF ROWS BY REMOVING THE ENTIRE TBODY SECTION
-
+    empTable.lastElementChild.remove();
     // REBUILD THE TBODY FROM SCRATCH
-
+    let tbody = document.createElement('tbody');
     // LOOP THROUGH THE ARRAY OF EMPLOYEES
     // REBUILDING THE ROW STRUCTURE
-
+    for (let employee of arrEmployees) {
+        tbody.innerHTML += 
+        `
+        <tr>
+            <td>${employee[0]}</td>
+            <td>${employee[1]}</td>
+            <td>${employee[2]}</td>
+            <td><a href="mailto:${employee[3]}">${employee[3]}</a></td>
+            <td>${employee[4]}</td>
+            <td><button class="btn btn-sm btn-danger delete">X</button></td>
+        </tr>
+        `
+    }
     // BIND THE TBODY TO THE EMPLOYEE TABLE
-
+    empTable.appendChild(tbody);
     // UPDATE EMPLOYEE COUNT
-
+    empCount.value = `(${arrEmployees.length})`;
     // STORE THE ARRAY IN STORAGE
-
+    localStorage.setItem('employees', JSON.stringify(arrEmployees));
+    //localStorage.setObj('employees', arrEmployees);
 };
