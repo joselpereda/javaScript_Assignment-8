9ce89b54f21afb6bfab9decccf2ec4d371fb7551
Jose Pereda <joselpereda@hotmail.com>
2023-04-11 09:24:08 -0700

    completed step 3

--- js/script-array-practice.js
+++ js/script-array-practice.js
@@ -1,10 +1,21 @@
 //STEP 1
 /* Create a string array that contains your five favorite movies. Then, use the console to display the second movie in your array. */
-let arrayMyFavoriteMovies = ['Casino', 'Braveheart', 'Good Fellas', 'The Matrix', 'Heat', 'Scicario'];
-console.log(arrayMyFavoriteMovies[1]);
+// let arrayMyFavoriteMovies = ['Casino', 'Braveheart', 'Good Fellas', 'The Matrix', 'Heat', 'Scicario'];
+// console.log(arrayMyFavoriteMovies[1]);
 
 //STEP 2
 /* Declare an array called movies using the function constructor method. Add the length of 5 into the constructor. Then, assign one of your favorite movies to each index in the array until you have 5 total movies in your array. Then, use the console to display the first movie in your array. */
+// let Movies = new Array(5);
+// Movies[0] = 'Casino'; 
+// Movies[1] = 'Braveheart';
+// Movies[2] = 'Good Fellas';
+// Movies[3] = 'The Matrix';
+// Movies[4] = 'Heat';
+
+// console.log(Movies[0]);
+
+//STEP 3
+/* Copy your code from step 2. Add a new movie into the 3rd position within your array. Then, use the console to display the length of the array. You should now have 6 total movies stored in the array. */
 let Movies = new Array(5);
 Movies[0] = 'Casino'; 
 Movies[1] = 'Braveheart';
@@ -12,10 +23,11 @@ Movies[2] = 'Good Fellas';
 Movies[3] = 'The Matrix';
 Movies[4] = 'Heat';
 
-console.log(Movies[0]);
+// Addd new movie to array in 3rd position
+Movies.splice(2,0,'Scicario');
 
-
-//STEP 3
+// Display new length of the Movies array
+console.log(Movies.length);
 
 //STEP 4
 
