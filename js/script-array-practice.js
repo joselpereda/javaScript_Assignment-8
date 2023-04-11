313de5880d3b6dcd87fab95a7fbc4329ece51a19
Jose Pereda <joselpereda@hotmail.com>
2023-04-11 10:44:49 -0700

    completed step 5

--- js/script-array-practice.js
+++ js/script-array-practice.js
@@ -16,24 +16,45 @@
 
 //STEP 3
 /* Copy your code from step 2. Add a new movie into the 3rd position within your array. Then, use the console to display the length of the array. You should now have 6 total movies stored in the array. */
-let Movies = new Array(5);
-Movies[0] = 'Casino'; 
-Movies[1] = 'Braveheart';
-Movies[2] = 'Good Fellas';
-Movies[3] = 'The Matrix';
-Movies[4] = 'Heat';
+// let Movies = new Array(5);
+// Movies[0] = 'Casino'; 
+// Movies[1] = 'Braveheart';
+// Movies[2] = 'Good Fellas';
+// Movies[3] = 'The Matrix';
+// Movies[4] = 'Heat';
 
 // Addd new movie to array in 3rd position
-Movies.splice(2,0,'Scicario');
+// Movies.splice(2,0,'Scicario');
 
 // Display new length of the Movies array
-console.log(Movies.length);
+// console.log(Movies.length);
 
 //STEP 4
+/* Declare an array called movies using literal notation. Then, assign one of your favorite movies to each index in the array until you have 5 total movies in your array. Now, use the delete operator to remove the first movie in the array. Use the console to display the contents of the array. */
+// let Movies = ['Casino', 'Braveheart', 'Good Fellas', 'The Matrix', 'Heat'];
+// delete Movies[0];
+// console.log(Movies);
 
 //STEP 5
+/* Declare an array called movies using literal notation. Then, assign one of your favorite movies to each index in the array until you have 7 total movies in your array. Now, use a for/in loop to iterate through the array and display each movie within the console window. */
+// let Movies = [];
+// Movies[0] = 'Casino'; 
+// Movies[1] = 'Braveheart';
+// Movies[2] = 'Good Fellas';
+// Movies[3] = 'The Matrix';
+// Movies[4] = 'Heat';
+// Movies[5] = 'Scicario';
+// Movies[6] = 'Never Ending Story';
+
+// for (let i in Movies) {
+//     if (Movies.hasOwnProperty(i)) {
+//         console.log(Movies[i]);
+//     }
+// }
 
 //STEP 6
+/* Copy the code from step 5. Now, use a for/of loop to iterate through the array and display each movie within the console window. */
+
 
 //STEP 7
 
